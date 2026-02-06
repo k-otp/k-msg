4b7352d86b5749733dc94ace17fcf6bd7cd244ef
imjlk <i@m.junglei.kim>
2026-02-07 00:30:56 +0900

    chore(messaging): update exports and remove legacy types
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-opencode)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- packages/messaging/src/index.ts
+++ packages/messaging/src/index.ts
@@ -1,32 +1,10 @@
+export { KMsg } from './k-msg';
+
 // Types
 export * from './types/message.types';
 
 // Senders
-export { SingleMessageSender, type Provider, type ProviderMessageRequest, type ProviderMessageResult } from './sender/single.sender';
 export { BulkMessageSender } from './sender/bulk.sender';
-
-// Services (기존)
-export { MessageService } from './services/message.service';
-
-// Services (새로운 공통 모듈)
-export { 
-  BaseMessageService, 
-  type MessageServiceConfig, 
-  type ServiceState,
-  type ChannelLoaderFn,
-  type TemplateLoaderFn,
-  type ErrorHandlerFn
-} from './services/base-message.service';
-
-export { 
-  IWINVMessageService, 
-  type IWINVMessageServiceConfig 
-} from './services/iwinv-message.service';
-
-export { 
-  MessageServiceFactory,
-  type ServiceFactoryOptions
-} from './services/message-service.factory';
 
 // Queue system
 export { JobProcessor, MessageJobProcessor } from './queue/job.processor';
@@ -38,5 +16,4 @@ export { DeliveryTracker } from './delivery/tracker';
 // Personalization
 export { VariableReplacer, defaultVariableReplacer, VariableUtils } from './personalization/variable.replacer';
 
-export { KMsg } from './k-msg';
 export * from './hooks';
